5b21c4df89ad2ba0aa3fc3ff72c2a1278be157a7
enginix <wnd1024@gmail.com>
2015-08-12 20:44:19 +0800

    make websocket.server optional

--- src/websocket.lua
+++ src/websocket.lua
@@ -1,12 +1,16 @@
 local frame = require'websocket.frame'
 
-return {
-  client = require'websocket.client',
-  server = require'websocket.server',
+return setmetatable({
   CONTINUATION = frame.CONTINUATION,
   TEXT = frame.TEXT,
   BINARY = frame.BINARY,
   CLOSE = frame.CLOSE,
   PING = frame.PING,
   PONG = frame.PONG
-}
+}, {__index = function(self, name)
+  if name == 'client' or name == 'server' then
+	local backend = require("websocket." .. name)
+    self[name] = backend
+	return backend
+  end
+end})
